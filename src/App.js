0cb0b3d205c8fd0045e73033e2c1b2a9f5f4519c
IMTIAZ <c193067@ugrad.iiuc.ac.bd>
2023-07-19 21:40:10 +0600

    component,props,dynamic value

--- src/App.js
+++ src/App.js
@@ -2,24 +2,72 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person={fname:'Syeed',sname:'Imtiaz'};
+  var style={color:'gold'};
+
+  const products=[
+    {name:"Jacket",price:"170"},
+    {name:"T-shirt",price:"150"}
+  ]
+
+
+
   return (
     <div className="App">
+  
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={style}>
+        
+        My name is {person.fname+" "+person.sname} .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{color:'lightblue'}}>My App</p>
+        <Person></Person> 
+        <Props course="React" teacher="Jhnkr"></Props>
       </header>
+      <ProductCard product={products[0]}></ProductCard>
     </div>
+     
   );
 }
+
+function Person() {
+
+  const pstyle={
+    margin:'10px',
+    border:'2px solid gold',
+    padding:'15px'
+  }
+
+  return (
+    <div>
+      <h1 style={pstyle}>React</h1>
+    </div>
+  )
+}
+
+function Props(pr){
+  return (
+    <div>
+     <h3>Course name: {pr.course}</h3> 
+     <h3>Instructor :{pr.teacher}</h3> 
+
+    </div>
+  )
+}
+
+function ProductCard(props){
+  
+  const {name,price}=props.product ;//Destructure 
+  return(
+  <div>
+    { <h1>{name}</h1>/* De-structure */ }
+    <h2>{props.product.price}</h2>
+  </div>
+
+)
+}
+
+
 
 export default App;
